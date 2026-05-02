9905f396dcfdacc920283afe0167f1daa1209ac5
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 10:44:58 -0500

    feat: added version checking.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,14 +3,18 @@ lua54("yes")
 fx_version("cerulean")
 game("gta5")
 
+version("1.0.3")
+repository("https://www.github.com/PulsarFW/pulsar-ws")
+
 server_only("yes")
 
 server_scripts({
-	"*.js",
-    "namespaces/*.js",
+  "*.js",
+  "namespaces/*.js",
+  "server/main.lua"
 })
 
 files {
-    "cert.pem",
-    "key.pem"
+  "cert.pem",
+  "key.pem"
 }
